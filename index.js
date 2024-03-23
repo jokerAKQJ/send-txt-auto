0e5a8a4efd96b187ef7d962df9da9aea0b81687d
jokerAKQJ <163806536+jokerAKQJ@users.noreply.github.com>
2024-03-23 13:42:08 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+from v_wallet import sendTransaction
+
+# Number of trans
+trans = 100
+
+# Generate 10 Cosmos wallets
+txts = [sendTransaction() for _ in range(trans)]
+
+# Print out the txt
+for i, txt in enumerate(txts):
+    print(f"txt {i+1}:")
+    print("Result:", txt.result)
+    print()
